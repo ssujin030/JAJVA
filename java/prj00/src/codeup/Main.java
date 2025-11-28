85053d689153c8daec1827e4801e391ee00b08d6
ssujin030 <134828538+ssujin030@users.noreply.github.com>
2025-11-28 18:30:59 +0900

    Add files via upload

---
+++ java/prj00/src/codeup/Main.java
@@ -0,0 +1,25 @@
+package codeup;
+
+import java.util.Scanner; 
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		if(x < 10) {
+			System.out.println("small");
+		}else{
+			System.out.println("big");
+				
+			}
+		}
+
+}
+	
+
+
+
+
---
+++ java/prj06control/src/loop/Main.java
@@ -0,0 +1,25 @@
+package loop;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 반복문
+		 */
+		
+//		int x = 0;
+//		while(x < 10) {
+//			System.out.println("Hello~~~");
+//			x++;
+//			
+		
+		for(int x = 0; x < 10; x++) {
+			System.out.println("hello");
+		}
+		
+		
+
+	}
+
+}
