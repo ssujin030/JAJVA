0cdd9a5cc654c6258da24d0897d182fb1b085a63
ssujin030 <134828538+ssujin030@users.noreply.github.com>
2025-12-05 17:38:21 +0900

    Add files via upload

---
+++ pratice09_vo/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("====Value Object====");		
+		
+		Member m=new Member("user01", "1234", "idragon");
+		
+		m.printInfo();
+		
+		
+
+	}
+
+}
